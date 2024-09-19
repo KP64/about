c186e0a1ae696effd8f0d7b380fea1c070275eeb
KP64 <karamalsadeh@hotmail.com>
2024-09-19 18:52:31 +0200

    feat: :sparkles: Nix Spinning

--- src/lib/components/Scene.svelte
+++ src/lib/components/Scene.svelte
@@ -1,35 +1,43 @@
 <script lang="ts">
-  import { T } from "@threlte/core";
-  import { ContactShadows, Float, Grid, OrbitControls } from "@threlte/extras";
+  import { T, useTask } from "@threlte/core";
+  import { OrbitControls, Grid, interactivity } from "@threlte/extras";
+  import { DEG2RAD } from "three/src/math/MathUtils.js";
+
   import Nix from "./models/Nix.svelte";
-  import Typescript from "./models/Typescript.svelte";
-  import Threlte from "./models/threlte.svelte";
+
+  interactivity();
+
+  let rotation = [90 * DEG2RAD, 0, 0];
+  let hovered = false;
+
+  useTask((delta) => {
+    if (hovered) return;
+    rotation[0] += delta * 90 * DEG2RAD;
+  });
 </script>
 
-<T.PerspectiveCamera makeDefault position={[-10, 10, 10]} fov={15}>
-  <OrbitControls enableZoom={true} enableDamping target.y={1.5} />
-</T.PerspectiveCamera>
+<Grid cellColor="#ffffff" sectionColor="#ffffff" fadeDistance={25} />
 
 <T.DirectionalLight intensity={0.8} position.x={5} position.y={10} />
 <T.AmbientLight intensity={0.2} />
 
-<Grid
-  position.y={-0.001}
-  cellColor="#ffffff"
-  sectionColor="#ffffff"
-  sectionThickness={0}
-  fadeDistance={25}
-  cellSize={2}
+<T.PerspectiveCamera
+  makeDefault
+  position.y={1}
+  position.z={5}
+  fov={95}
+  on:create={({ ref }) => ref.lookAt(0, 1, 5)}
+>
+  <OrbitControls enableDamping target.y={1} />
+</T.PerspectiveCamera>
+
+<Nix
+  scale={20}
+  mater
+  {rotation}
+  on:pointerenter={() => {
+    rotation = [90 * DEG2RAD, 0, 0];
+    hovered = true;
+  }}
+  on:pointerleave={() => (hovered = false)}
 />
-
-<Float floatIntensity={1} floatingRange={[0, 1]}>
-  <Nix scale={20} rotation={[45, 0, 0]} position={[1, 0, -3]} />
-</Float>
-
-<Float floatIntensity={1} floatingRange={[0, 1]}>
-  <Typescript scale={20} />
-</Float>
-
-<Threlte scale={0.5} position={[3, 0, -3]} rotation={[0, 90, 0]} />
-
-<ContactShadows scale={10} blur={2} far={2.5} opacity={0.5} />
